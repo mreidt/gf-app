161535639d87571a65733bec38363e067096081e
Matheus Eidt <matheus@mx2.unisc.br>
2021-07-06 18:50:11 -0300

    closes #15 and #16 - Created and testes user management

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework.authtoken',
+    'user'
 ]
 
 MIDDLEWARE = [
